8a309a3e4f47f90e6ce0e651ae22111f831c5cc4
jameslunatic <jjluna@gbox.adnu.edu.ph>
2020-10-22 02:18:17 +0800

    Updated settings_screen

--- lib/settings_screen.dart
+++ lib/settings_screen.dart
@@ -29,328 +29,99 @@ class settingsPage extends StatelessWidget{
             //padding: EdgeInsets.all(10),
             children: <Widget>[
               Container(
-                margin: EdgeInsets.fromLTRB(50, 20, 50, 40),
-                child: Align(
-                  child: Image.asset('images/logo3.png',),
-
-                ),
+                margin: EdgeInsets.fromLTRB(50, 10, 50, 40),
+                child: Image.asset('images/logo3.png',),
               ),
               Container(
-                padding:  EdgeInsets.fromLTRB(50, 3, 0, 3),
-                margin:  EdgeInsets.fromLTRB(0, 0, 0, 30),
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      child: Icon(
-                        Icons.account_circle,
-                        size: 50,
-                        color: Colors.grey[500],
-                      ),
-                    ),
-                    Container(
-                      child: RaisedButton(
-                        color: Colors.white,
-                        //disabledColor: ,
-                        padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                margin:  EdgeInsets.fromLTRB(0, 0, 0, 0),
+                alignment: Alignment.topLeft,
+                child: Column(
+                    children: <Widget> [
+                      FlatButton(
+                        textColor: Colors.grey,
                         child: Text(
-                          "What's on your mind?",
+                          "Account",
                           style: TextStyle(
                             fontSize: 18,
-                            color: Colors.grey,
                           ),
                         ),
                         onPressed: (){
-
+                          //button
                         },
-
                       ),
-                    ),
-                  ],
-                ),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black)
+                      Container(
+                        margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                        padding: EdgeInsets.fromLTRB(150, 0, 120, 0),
+                        decoration: BoxDecoration(
+                          border: Border(
+                              bottom: BorderSide(width: 2.0, color: Colors.grey)
+                          ),
+                        ),
+                      ),
+                    ]
                 ),
               ),
               Container(
-                padding:  EdgeInsets.fromLTRB(0, 0, 0, 0),
+                margin:  EdgeInsets.fromLTRB(0, 25, 0, 0),
+                alignment: Alignment.topLeft,
                 child: Column(
-                  children: <Widget> [
-                    Container(
-                      child: Column(
-                        children: <Widget> [
-                          Container(
-                            margin: EdgeInsets.fromLTRB(380, 10, 0, 0),
-                            child: Icon(
-                              Icons.keyboard_arrow_down,
-                              color: Colors.grey[900],
-                            ),
+                    children: <Widget> [
+                      FlatButton(
+                        textColor: Colors.grey,
+                        child: Text(
+                          "Privacy",
+                          style: TextStyle(
+                            fontSize: 18,
                           ),
-                          Container(
-                            margin:  EdgeInsets.fromLTRB(0, 0, 0, 0),
-                            child:  Row(
-                              children: <Widget>[
-                                Container(
-                                  margin: EdgeInsets.fromLTRB(5, 0, 0, 20),
-                                  padding:  EdgeInsets.fromLTRB(0, 0, 0, 0),
-                                  child: Icon(
-                                    Icons.account_box,
-                                    size: 95,
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                                Container(
-                                  child: Column(
-                                    children: <Widget> [
-                                      Container(
-                                        margin: EdgeInsets.fromLTRB(10, 0, 0, 25),
-                                        padding: EdgeInsets.fromLTRB(150, 0, 120, 0),
-                                        decoration: BoxDecoration(
-                                            border: Border(
-                                                bottom: BorderSide(width: 2.0, color: Colors.grey)
-                                            )
-                                        ),
-                                      ),
-                                      Container(
-                                        margin: EdgeInsets.fromLTRB(10, 0, 0, 25),
-                                        padding: EdgeInsets.fromLTRB(150, 0, 120, 0),
-                                        decoration: BoxDecoration(
-                                            border: Border(
-                                                bottom: BorderSide(width: 2.0, color: Colors.grey)
-                                            )
-                                        ),
-                                      ),
-                                      Container(
-                                        margin: EdgeInsets.fromLTRB(10, 0, 0, 25),
-                                        padding: EdgeInsets.fromLTRB(150, 0, 120, 0),
-                                        decoration: BoxDecoration(
-                                            border: Border(
-                                                bottom: BorderSide(width: 2.0, color: Colors.grey)
-                                            )
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                )
-                              ],
-                            ),
-                          )
-                        ],
+                        ),
+                        onPressed: (){
+                          // Home
+                        },
                       ),
-                      decoration: BoxDecoration(
+                      Container(
+                        margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                        padding: EdgeInsets.fromLTRB(150, 0, 120, 0),
+                        decoration: BoxDecoration(
                           border: Border(
-                              bottom: BorderSide(width: 1.0)
-                          )
-                      ),
-                    ),
-                    Container(
-                      child: Column(
-                        children: <Widget> [
-                          Container(
-                            margin: EdgeInsets.fromLTRB(380, 10, 0, 0),
-                            child: Icon(
-                              Icons.keyboard_arrow_down,
-                              color: Colors.grey[900],
-                            ),
+                              bottom: BorderSide(width: 2.0, color: Colors.grey)
                           ),
-                          Container(
-                            margin:  EdgeInsets.fromLTRB(0, 0, 0, 0),
-                            child:  Row(
-                              children: <Widget>[
-                                Container(
-                                  margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
-                                  padding:  EdgeInsets.fromLTRB(0, 0, 0, 20),
-                                  child: Icon(
-                                    Icons.account_box,
-                                    size: 95,
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                                Container(
-                                  child: Column(
-                                    children: <Widget> [
-                                      Container(
-                                        margin: EdgeInsets.fromLTRB(10, 0, 0, 25),
-                                        padding: EdgeInsets.fromLTRB(150, 0, 120, 0),
-                                        decoration: BoxDecoration(
-                                            border: Border(
-                                                bottom: BorderSide(width: 2.0, color: Colors.grey)
-                                            )
-                                        ),
-                                      ),
-                                      Container(
-                                        margin: EdgeInsets.fromLTRB(10, 0, 0, 25),
-                                        padding: EdgeInsets.fromLTRB(150, 0, 120, 0),
-                                        decoration: BoxDecoration(
-                                            border: Border(
-                                                bottom: BorderSide(width: 2.0, color: Colors.grey)
-                                            )
-                                        ),
-                                      ),
-                                      Container(
-                                        margin: EdgeInsets.fromLTRB(10, 0, 0, 25),
-                                        padding: EdgeInsets.fromLTRB(150, 0, 120, 0),
-                                        decoration: BoxDecoration(
-                                            border: Border(
-                                                bottom: BorderSide(width: 2.0, color: Colors.grey)
-                                            )
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                )
-                              ],
-                            ),
-                          )
-                        ],
+                        ),
                       ),
-                      decoration: BoxDecoration(
-                          border: Border(
-                              bottom: BorderSide(width: 1.0)
-                          )
-                      ),
-                    ),
-                    Container(
-                      child: Column(
-                        children: <Widget> [
-                          Container(
-                            margin: EdgeInsets.fromLTRB(380, 10, 0, 0),
-                            child: Icon(
-                              Icons.keyboard_arrow_down,
-                              color: Colors.grey[900],
-                            ),
-                          ),
-                          Container(
-                            margin:  EdgeInsets.fromLTRB(0, 0, 0, 0),
-                            child:  Row(
-                              children: <Widget>[
-                                Container(
-                                  margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
-                                  padding:  EdgeInsets.fromLTRB(0, 0, 0, 20),
-                                  child: Icon(
-                                    Icons.account_box,
-                                    size: 95,
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                                Container(
-                                  child: Column(
-                                    children: <Widget> [
-                                      Container(
-                                        margin: EdgeInsets.fromLTRB(10, 0, 0, 25),
-                                        padding: EdgeInsets.fromLTRB(150, 0, 120, 0),
-                                        decoration: BoxDecoration(
-                                            border: Border(
-                                                bottom: BorderSide(width: 2.0, color: Colors.grey)
-                                            )
-                                        ),
-                                      ),
-                                      Container(
-                                        margin: EdgeInsets.fromLTRB(10, 0, 0, 25),
-                                        padding: EdgeInsets.fromLTRB(150, 0, 120, 0),
-                                        decoration: BoxDecoration(
-                                            border: Border(
-                                                bottom: BorderSide(width: 2.0, color: Colors.grey)
-                                            )
-                                        ),
-                                      ),
-                                      Container(
-                                        margin: EdgeInsets.fromLTRB(10, 0, 0, 25),
-                                        padding: EdgeInsets.fromLTRB(150, 0, 120, 0),
-                                        decoration: BoxDecoration(
-                                            border: Border(
-                                                bottom: BorderSide(width: 2.0, color: Colors.grey)
-                                            )
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                )
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                      decoration: BoxDecoration(
-                          border: Border(
-                              bottom: BorderSide(width: 1.0)
-                          )
-                      ),
-                    ),
-                    Container(
-                      child: Column(
-                        children: <Widget> [
-                          Container(
-                            margin: EdgeInsets.fromLTRB(380, 10, 0, 0),
-                            child: Icon(
-                              Icons.keyboard_arrow_down,
-                              color: Colors.grey[900],
-                            ),
-                          ),
-                          Container(
-                            margin:  EdgeInsets.fromLTRB(0, 0, 0, 0),
-                            child:  Row(
-                              children: <Widget>[
-                                Container(
-                                  margin: EdgeInsets.fromLTRB(5, 0, 0, 0),
-                                  padding:  EdgeInsets.fromLTRB(0, 0, 0, 20),
-                                  child: Icon(
-                                    Icons.account_box,
-                                    size: 95,
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                                Container(
-                                  child: Column(
-                                    children: <Widget> [
-                                      Container(
-                                        margin: EdgeInsets.fromLTRB(10, 0, 0, 25),
-                                        padding: EdgeInsets.fromLTRB(150, 0, 120, 0),
-                                        decoration: BoxDecoration(
-                                            border: Border(
-                                                bottom: BorderSide(width: 2.0, color: Colors.grey)
-                                            )
-                                        ),
-                                      ),
-                                      Container(
-                                        margin: EdgeInsets.fromLTRB(10, 0, 0, 25),
-                                        padding: EdgeInsets.fromLTRB(150, 0, 120, 0),
-                                        decoration: BoxDecoration(
-                                            border: Border(
-                                                bottom: BorderSide(width: 2.0, color: Colors.grey)
-                                            )
-                                        ),
-                                      ),
-                                      Container(
-                                        margin: EdgeInsets.fromLTRB(10, 0, 0, 25),
-                                        padding: EdgeInsets.fromLTRB(150, 0, 120, 0),
-                                        decoration: BoxDecoration(
-                                            border: Border(
-                                                bottom: BorderSide(width: 2.0, color: Colors.grey)
-                                            )
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                )
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                      decoration: BoxDecoration(
-                          border: Border(
-                              bottom: BorderSide(width: 1.0)
-                          )
-                      ),
-                    ),
-                  ],
-                ),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black)
+                    ]
                 ),
               ),
-
+              Container(
+                margin:  EdgeInsets.fromLTRB(0, 25, 0, 0),
+                alignment: Alignment.topLeft,
+                child: FlatButton(
+                  textColor: Colors.grey,
+                  child: Text(
+                    "Block",
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  onPressed: (){
+                    // Home
+                  },
+                ),
+              ),
+              Container(
+                margin:  EdgeInsets.fromLTRB(0, 25, 0, 0),
+                alignment: Alignment.topLeft,
+                child: FlatButton(
+                  textColor: Colors.grey,
+                  child: Text(
+                    "Help",
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  onPressed: (){
+                    //home
+                  },
+                ),
+              ),
             ],
           ),
         ),
