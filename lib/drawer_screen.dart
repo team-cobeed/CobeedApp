02375d7d09c6ccb35914528c25f5f53439e81a62
Katoari <anselmood06@gmail.com>
2020-10-21 18:15:22 +0800

    10/21/2020 Ansel's update

---
+++ lib/drawer_screen.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+import 'index_screen.dart';
+
+class drawerPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 500,
+      child: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget> [
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 40, 350, 0),
+              child: IconButton(icon: Icon(
+                Icons.arrow_back,
+
+              ),
+                onPressed: (){
+                  Navigator.pop(context, false);
+                },
+              ),
+            ),
+            Container(
+                margin:  EdgeInsets.fromLTRB(0, 30, 0, 0),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      margin:  EdgeInsets.fromLTRB(0, 0, 0, 10),
+                      child: Icon(
+                        Icons.account_circle,
+                        size: 75,
+                        color: Colors.grey[500],
+                      ),
+                    ),
+                    Container(
+                      child: Text(
+                        "Anselmo Oduca",
+                        style: TextStyle(
+                          fontSize: 26,
+                          color: Colors.grey[500],
+                        ),
+                      ),
+                    )
+                  ],
+                )
+            ),
+            Container(
+              margin:  EdgeInsets.fromLTRB(0, 70, 0, 0),
+              child: FlatButton(
+                textColor: Colors.grey,
+                child: Text(
+                  "Home",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                onPressed: (){
+                  Navigator.pop(context, false);
+                },
+              ),
+            ),
+            Container(
+              margin:  EdgeInsets.fromLTRB(0, 25, 0, 0),
+              child: FlatButton(
+                textColor: Colors.grey,
+                child: Text(
+                  "Doctor's Appointment",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                onPressed: (){
+                  // Home
+                },
+              ),
+            ),
+            Container(
+              margin:  EdgeInsets.fromLTRB(0, 25, 0, 0),
+              child: FlatButton(
+                textColor: Colors.grey,
+                child: Text(
+                  "Profile",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                onPressed: (){
+                  // Home
+                },
+              ),
+            ),
+            Container(
+              margin:  EdgeInsets.fromLTRB(0, 25, 0, 0),
+              child: FlatButton(
+                textColor: Colors.grey,
+                child: Text(
+                  "Settings",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                onPressed: (){
+                  // Home
+                },
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
+              child: RaisedButton(
+                color: Colors.grey,
+                padding: EdgeInsets.all(23),
+                child: Text(
+                  'Sign Out',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(
+                      builder: (context)=>IndexScreen()
+                  ));
+                },
+                //color: Colors.grey,
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cobeed_app/drawer_screen.dart';
 import 'package:flutter/material.dart';
 import 'index_screen.dart';
 
@@ -12,25 +13,83 @@ class homePage extends StatelessWidget{
             color: Colors.black, //change your color here
             size: 100.00,
           ),
-        ),
-        drawer: Container(
-          width: 500,
-          child: Drawer(
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: <Widget> [
-                  Container(
-                    margin: EdgeInsets.fromLTRB(0, 40, 350, 0),
-                    child: IconButton(icon: Icon(
-                            Icons.arrow_back,
+          actions: <Widget>[
+            Container(
+              child: IconButton(
+                icon: Icon(
+                  Icons.perm_identity,
+                  size: 40,
+                ),
+                onPressed: () {
+                  // do something
+                },
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.fromLTRB(10, 16, 12, 16),
+                child: Text(
+                  "Anselmo",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[600],
+                  ),
+                ),
+            )
 
-                      ),
+          ],
+        ),
+          body: Center(
+            child: ListView(
+              padding: EdgeInsets.all(10),
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.fromLTRB(100, 30, 0, 15),
+                  child: Text(
+                    "QUARANTINE",
+                    style: TextStyle(
+                      fontSize: 30,
+                      decoration: TextDecoration.underline,
                     ),
                   ),
+                ),
+                Container(
+                  margin:  EdgeInsets.fromLTRB(80, 0, 80, 0),
+                  padding:  EdgeInsets.fromLTRB(0, 40,0, 50),
+                  child: Column(
+                    children: <Widget> [
+                      Container(
+                        child: Text(
+                          "0 Days",
+                          style: TextStyle(
+                            fontSize: 40,
+                            decoration: TextDecoration.underline,
+                          ),
+
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 10),
+                        child: Text(
+                          "You're Safe",
+                          style: TextStyle(
+                            fontSize: 20,
+
+                        ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(160),
+                      border: Border.all(color: Colors.grey)
+                  ),
+                )
               ],
             ),
           ),
-        ),
+        drawer: drawerPage()
+
       );
   }
 }
