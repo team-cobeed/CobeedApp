90d11ed834b7e74220c4ddaa8f977089353c937b
Brandon Sarte <bsarte@gbox.adnu.edu.ph>
2020-10-23 02:48:32 +0800

    Brandog Updates (Profile Complete)
    nice

--- lib/profile_2.dart
+++ lib/profile_2.dart
@@ -1,5 +1,7 @@
+import 'package:cobeed_app/profile_1.dart';
 import 'package:flutter/material.dart';
 import 'package:cobeed_app/drawer_screen.dart';
+import 'profile_1.dart';
 
 class profile_2 extends StatelessWidget{
   @override
@@ -13,8 +15,214 @@ class profile_2 extends StatelessWidget{
           size: 100.00,
         ),
       ),
-      body: Text("hi"),
-      drawer: drawerPage(),
+      body: Center(
+       child: ListView(
+         children: <Widget>[
+           Container(
+             margin: const EdgeInsets.fromLTRB(40, 40, 40, 10),
+             child: Icon(
+               Icons.person,
+               size: 120,
+               color: Colors.grey[100],
+             ),
+             decoration: BoxDecoration(
+               borderRadius: BorderRadius.circular(60),
+               color: Colors.grey[400],
+
+               boxShadow: [
+                 BoxShadow(color: Colors.grey[400], spreadRadius: 1),
+               ],
+             ),
+             height: 200,
+           ),
+           Container(
+             margin: const EdgeInsets.fromLTRB(25, 5, 5, 10),
+             child: Text('Name',
+               textAlign: TextAlign.justify,
+               style: TextStyle(
+                 fontSize: 15,
+                 letterSpacing: 2,
+                 color: Colors.grey[600],
+               ),
+             ),
+           ),
+           Container(
+             padding: EdgeInsets.fromLTRB(25, 0, 25, 10),
+             margin: EdgeInsets.all(10),
+             child: TextField(
+
+               decoration: InputDecoration(
+                 border: OutlineInputBorder(),
+                 isDense: true,
+                 contentPadding: const EdgeInsets.all(15.0),
+                 labelText: 'Brandon Sarte',
+                 fillColor: Colors.grey[400],
+                   labelStyle: TextStyle(
+                     color: Colors.grey[500],
+                   )
+               ),
+             ),
+           ),
+           Container(
+             margin: const EdgeInsets.fromLTRB(25, 5, 5, 10),
+             child: Text('Email',
+               textAlign: TextAlign.justify,
+               style: TextStyle(
+                 fontSize: 15,
+                 letterSpacing: 2,
+                 color: Colors.grey[600],
+               ),
+             ),
+           ),
+           Container(
+             padding: EdgeInsets.fromLTRB(25, 0, 25, 0),
+             margin: EdgeInsets.all(10),
+             child: TextField(
+               obscureText: true,
+               decoration: InputDecoration(
+                 border: OutlineInputBorder(),
+                 isDense: true,
+                 contentPadding: const EdgeInsets.all(15.0),
+                 labelText: 'bsarte@gbox.adnu.edu.ph',
+                 fillColor: Colors.grey[400],
+                 labelStyle: TextStyle(
+                   color: Colors.grey[500],
+                 )
+               ),
+             ),
+           ),
+           Container(
+             margin: const EdgeInsets.fromLTRB(25, 5, 5, 10),
+             child: Text('Address',
+               textAlign: TextAlign.justify,
+               style: TextStyle(
+                 fontSize: 15,
+                 letterSpacing: 2,
+                 color: Colors.grey[600],
+               ),
+             ),
+           ),
+           Container(
+             padding: EdgeInsets.fromLTRB(25, 0, 25, 10),
+             margin: EdgeInsets.all(10),
+             child: TextField(
+
+               decoration: InputDecoration(
+                 border: OutlineInputBorder(),
+                 isDense: true,
+                 contentPadding: const EdgeInsets.all(15.0),
+                 labelText: 'Tinago, Naga City',
+                 fillColor: Colors.grey[400],
+                 labelStyle: TextStyle(
+                   color: Colors.grey[500],
+                 )
+
+               ),
+             ),
+           ),
+           Container(
+             margin: const EdgeInsets.fromLTRB(25, 5, 5, 10),
+             child: Text('Contact Number',
+               textAlign: TextAlign.justify,
+               style: TextStyle(
+                 fontSize: 15,
+                 letterSpacing: 2,
+                 color: Colors.grey[600],
+               ),
+             ),
+           ),
+           Container(
+             padding: EdgeInsets.fromLTRB(25, 0, 25, 0),
+             margin: EdgeInsets.all(10),
+             child: TextField(
+               obscureText: true,
+               decoration: InputDecoration(
+                 border: OutlineInputBorder(),
+                 isDense: true,
+                 contentPadding: const EdgeInsets.all(15.0),
+                 labelText: '+63 945 258 9614',
+                 labelStyle: TextStyle(
+                   color: Colors.grey[500],
+                 )
+               ),
+             ),
+           ),
+           Container(
+             margin: const EdgeInsets.fromLTRB(25, 5, 5, 10),
+             child: Text('Password',
+               textAlign: TextAlign.justify,
+               style: TextStyle(
+                 fontSize: 15,
+                 letterSpacing: 2,
+                 color: Colors.grey[600],
+               ),
+             ),
+           ),
+           Container(
+             padding: EdgeInsets.fromLTRB(25, 0, 25, 10),
+             margin: EdgeInsets.all(10),
+             child: TextField(
+
+               decoration: InputDecoration(
+                 border: OutlineInputBorder(),
+                 isDense: true,
+                 contentPadding: const EdgeInsets.all(15.0),
+               ),
+             ),
+           ),
+           Container(
+             margin: const EdgeInsets.fromLTRB(25, 5, 5, 10),
+             child: Text('Confirm Password',
+               textAlign: TextAlign.justify,
+               style: TextStyle(
+                 fontSize: 15,
+                 letterSpacing: 2,
+                 color: Colors.grey[600],
+               ),
+             ),
+           ),
+           Container(
+             padding: EdgeInsets.fromLTRB(25, 0, 25, 0),
+             margin: EdgeInsets.all(10),
+             child: TextField(
+               obscureText: true,
+               decoration: InputDecoration(
+                 border: OutlineInputBorder(),
+                 isDense: true,
+                 contentPadding: const EdgeInsets.all(15.0),
+               ),
+             ),
+           ),
+           Container(
+             height: 100,
+             child: Row(
+               children: <Widget> [
+                 Container(
+                     height: 50,
+                     width: 170,
+                     margin: EdgeInsets.fromLTRB(205, 0, 0, 0),
+                     padding: EdgeInsets.fromLTRB(50, 10, 0, 0),
+                     child: RaisedButton(
+                         textColor: Colors.white,
+                         color: Colors.grey[600],
+                         child: Text('Submit',
+                           style: TextStyle(
+                             letterSpacing: 2,
+                           ),
+                         ),
+                         onPressed: (){
+                           Navigator.push(context, MaterialPageRoute(
+                               builder: (context)=> profile_1()
+                           )); //Index Screen
+                         }
+                     )
+                 )
+               ],
+             ),
+           )
+         ],
+       )
+      ),
     );
   }
 }
--- lib/profile_inc.dart
+++ lib/profile_inc.dart
@@ -16,143 +16,145 @@ class profile_IncPage extends StatelessWidget{
             size: 100.00,
           ),
         ),
-        body: Center(
-            child: ListView(
-              children: <Widget>[
-                Container(
-                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
-                  height: 125,
-                  color: Colors.grey[500],
-                  child: Center(
-                    child: Icon(
-                      Icons.person,
-                      size: 120,
-                      color: Colors.grey[100],
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(25, 20, 5, 10),
-                  child: Text('Brandon Sarte',
-                    textAlign: TextAlign.justify,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.grey[600],
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(25, 5, 5, 10),
-                  child: Text('bsarte@gbox.adnu.edu.ph',
-                    textAlign: TextAlign.justify,
-                    style: TextStyle(
-                      fontSize: 13,
-                      letterSpacing: 2,
-                      color: Colors.grey[600],
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(15),
-                  child: Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Text(
-                      'To complete your profile please add the following.',
-
-                      textAlign: TextAlign.justify,
-                      style: TextStyle(
-                        color: Colors.grey[600],
-                        fontSize: 20,
+        body: Form(
+          child: Center(
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                    height: 125,
+                    color: Colors.grey[500],
+                    child: Center(
+                      child: Icon(
+                        Icons.person,
+                        size: 120,
+                        color: Colors.grey[100],
                       ),
                     ),
                   ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.white,
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(25, 20, 5, 10),
+                    child: Text('Brandon Sarte',
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(25, 5, 5, 10),
+                    child: Text('bsarte@gbox.adnu.edu.ph',
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(
+                        fontSize: 13,
+                        letterSpacing: 2,
+                        color: Colors.grey[600],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(15),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Text(
+                        'To complete your profile please add the following.',
 
-                    boxShadow: [
-                      BoxShadow(color: Colors.grey[500], spreadRadius: 1),
-                    ],
-                  ),
-                  height: 100,
-                ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(25, 5, 5, 10),
-                  child: Text('Address',
-                    textAlign: TextAlign.justify,
-                    style: TextStyle(
-                      fontSize: 15,
-                      letterSpacing: 2,
-                      color: Colors.grey[600],
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                          color: Colors.grey[600],
+                          fontSize: 20,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(25, 0, 25, 10),
-                  margin: EdgeInsets.all(10),
-                  child: TextField(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
 
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      isDense: true,
-                      contentPadding: const EdgeInsets.all(15.0),
+                      boxShadow: [
+                        BoxShadow(color: Colors.grey[500], spreadRadius: 1),
+                      ],
+                    ),
+                    height: 100,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(25, 5, 5, 10),
+                    child: Text('Address',
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(
+                        fontSize: 15,
+                        letterSpacing: 2,
+                        color: Colors.grey[600],
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  margin: const EdgeInsets.fromLTRB(25, 5, 5, 10),
-                  child: Text('Contact Number',
-                    textAlign: TextAlign.justify,
-                    style: TextStyle(
-                      fontSize: 15,
-                      letterSpacing: 2,
-                      color: Colors.grey[600],
+                  Container(
+                    padding: EdgeInsets.fromLTRB(25, 0, 25, 10),
+                    margin: EdgeInsets.all(10),
+                    child: TextField(
+
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        isDense: true,
+                        contentPadding: const EdgeInsets.all(15.0),
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(25, 0, 25, 0),
-                  margin: EdgeInsets.all(10),
-                  child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      isDense: true,
-                      contentPadding: const EdgeInsets.all(15.0),
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(25, 5, 5, 10),
+                    child: Text('Contact Number',
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(
+                        fontSize: 15,
+                        letterSpacing: 2,
+                        color: Colors.grey[600],
+                      ),
                     ),
                   ),
-                ),
-                Container(
-                  height: 100,
-                  child: Row(
-                    children: <Widget> [
-                      Container(
-                          height: 50,
-                          width: 170,
-                          margin: EdgeInsets.fromLTRB(205, 0, 0, 0),
-                          padding: EdgeInsets.fromLTRB(50, 10, 0, 0),
-                          child: RaisedButton(
-                              textColor: Colors.white,
-                              color: Colors.grey[600],
-                              child: Text('Submit',
-                                style: TextStyle(
-                                  letterSpacing: 2,
+                  Container(
+                    padding: EdgeInsets.fromLTRB(25, 0, 25, 0),
+                    margin: EdgeInsets.all(10),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        isDense: true,
+                        contentPadding: const EdgeInsets.all(15.0),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    child: Row(
+                      children: <Widget> [
+                        Container(
+                            height: 50,
+                            width: 170,
+                            margin: EdgeInsets.fromLTRB(205, 0, 0, 0),
+                            padding: EdgeInsets.fromLTRB(50, 10, 0, 0),
+                            child: RaisedButton(
+                                textColor: Colors.white,
+                                color: Colors.grey[600],
+                                child: Text('Submit',
+                                  style: TextStyle(
+                                    letterSpacing: 2,
+                                  ),
                                 ),
-                              ),
-                              onPressed: (){
-                                Navigator.push(context, MaterialPageRoute(
-                                    builder: (context)=> profile_1()
-                                )); //Index Screen
-                              }
-                          )
-                      )
-                    ],
-                  ),
-                )
+                                onPressed: (){
+                                  Navigator.push(context, MaterialPageRoute(
+                                      builder: (context)=> profile_1()
+                                  )); //Index Screen
+                                }
+                            )
+                        )
+                      ],
+                    ),
+                  )
 
-              ],
-            )
+                ],
+              )
+          ),
         ),
         drawer: drawerPage()
 
